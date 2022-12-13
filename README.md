505067d67ef55c847350a35f8632d00c9ab1104c
Joel Rose <mail@joel-rose.de>
2022-12-13 22:43:29 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Weather API
 
-The tool fetches the "Jahresmittel- und die Norm-Außentemperaturen" from the Bundesverband Wärmepumpe
+This tool is used to get the "Jahresmittel- und die Norm-Außentemperaturen" from the Bundesverband Wärmepumpe. 
+
+[Website](https://www.waermepumpe.de/normen-technik/klimakarte/)
+
+
+## Usage
+
+Update the files/results.html and run:
+
+```bash
+make run
+```
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/anaskhan96/soup"
 	"github.com/gocarina/gocsv"
@@ -10,27 +13,36 @@ import (
 
 const (
 	inputFileName  = "files/results.html"
-	outputFileName = "output.csv"
+	outputFileName = "files/output.csv"
 )
 
 func initLogger() {
+	log.SetLevel(log.DebugLevel)
 	log.SetFormatter(&log.TextFormatter{
-		ForceColors:   true,
-		FullTimestamp: true,
+		ForceColors: true,
 	})
 }
 
 type Record struct {
-	Postleitzahl           string `csv:"postleitzahl"`
-	Jahresmitteltemperatur string `csv:"jahresmitteltemperatur"`
-	NormAußentemperatur    string `csv:"normAußentemperatur"`
-	Hoehe                  string `csv:"hoehe"`
-	Klimazone              string `csv:"klimazone"`
-	Ort                    string `csv:"ort"`
+	Postleitzahl           string  `csv:"zip"`
+	Jahresmitteltemperatur float32 `csv:"meanAnnualTemperature"`
+	NormAußentemperatur    float32 `csv:"normOutsideTemperature"`
+	Hoehe                  int     `csv:"height"`
+	Klimazone              int     `csv:"zone"`
+	Ort                    string  `csv:"place"`
+}
+
+func cleanCelsius(s string) string {
+	return strings.Replace(s, " °C", "", 1)
+}
+
+func cleanMeter(s string) string {
+	return strings.Replace(s, " m", "", 1)
 }
 
 func main() {
 	initLogger()
+	log.Info("Running parser")
 
 	html, err := os.ReadFile(inputFileName)
 	if err != nil {
@@ -40,19 +52,49 @@ func main() {
 	doc := soup.HTMLParse(string(html))
 	elements := doc.FindAll("polygon")
 
+	var sb strings.Builder
+	sb.WriteString("INSERT INTO weather_data (zip, mean_annual_temperature, norm_outside_temperature, height, zone, place) VALUES")
 	records := []Record{}
+
+	seenElements := map[string]bool{}
 	for _, element := range elements {
 		attrs := element.Attrs()
 
-		records = append(records, Record{
-			Postleitzahl:           attrs["zip"],
-			Jahresmitteltemperatur: attrs["aat"],
-			NormAußentemperatur:    attrs["dot"],
-			Hoehe:                  attrs["alt"],
-			Klimazone:              attrs["zone"],
-			Ort:                    attrs["place"],
-		})
+		zip := attrs["zip"]
+		if seenElements[zip] {
+			continue
+		} else {
+			seenElements[zip] = true
+		}
 
+		jat, err := strconv.ParseFloat(cleanCelsius(attrs["aat"]), 16)
+		if err != nil {
+			log.WithError(err).Fatal("failed converting aat")
+		}
+		nat, err := strconv.ParseFloat(cleanCelsius(attrs["dot"]), 16)
+		if err != nil {
+			log.WithError(err).Fatal("failed converting dot")
+		}
+		height, err := strconv.Atoi(cleanMeter(attrs["alt"]))
+		if err != nil {
+			log.WithError(err).Fatal("failed converting alt")
+		}
+		zone, err := strconv.Atoi(attrs["zone"])
+		if err != nil {
+			log.WithError(err).Fatal("failed converting zone")
+		}
+		place := attrs["place"]
+
+		sb.WriteString(fmt.Sprintf("('%v', %v, %v, %v, %v, '%v'),", zip, jat, nat, height, zone, place))
+
+		records = append(records, Record{
+			Postleitzahl:           zip,
+			Jahresmitteltemperatur: float32(jat),
+			NormAußentemperatur:    float32(nat),
+			Hoehe:                  height,
+			Klimazone:              zone,
+			Ort:                    place,
+		})
 	}
 
 	file, err := os.Create(outputFileName)
@@ -69,4 +111,10 @@ func main() {
 	if err != nil {
 		log.WithError(err).Error("failed to write to csv")
 	}
+
+	sqlHandle, _ := os.Create("test.sql")
+
+	sqlHandle.WriteString(sb.String())
+
+	log.Infof("Parser executed successful, check the %s file for the results.", outputFileName)
 }
